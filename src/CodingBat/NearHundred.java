bde1050115ec922a20edbd7ff9be864c771c1444
OnrHng <hangul07@hacettepe.edu.tr>
2021-06-05 19:18:38 +0200

    CodingBat some code solutions

---
+++ src/CodingBat/NearHundred.java
@@ -0,0 +1,25 @@
+package CodingBat;
+
+public class NearHundred {
+//
+//  Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+//
+//
+//  nearHundred(93) → true
+//  nearHundred(90) → true
+//  nearHundred(89) → false
+
+  public static void main(String[] args) {
+    System.out.println(nearHundred(93));
+    System.out.println(nearHundred(90));
+    System.out.println(nearHundred(89));
+  }
+
+  public static boolean nearHundred(int n) {
+
+    if ( (n >= 90 && n <=110) || (n >= 190 && n <= 210)) return true;
+
+    return false;
+  }
+
+}
